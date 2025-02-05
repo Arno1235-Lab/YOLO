15808cf008036b0bad7d73bfbb9cfe47a9039675
Arno Van Eetvelde <arno.ve2000@gmail.com>
2025-02-05 18:04:24 +0100

    eval

--- evaluate/evaluate.py
+++ evaluate/evaluate.py
@@ -1,14 +1,33 @@
+import sys
+sys.path.append('..')
+from utils import set_mlflow_tracking
+import argparse
+import yaml
 from ultralytics import YOLO
 
-# Load a model
-model = YOLO("001-IS-MVTec/train/weights/best.pt")
 
-# TODO: run validation on test set
+if __name__ == '__main__':
+    set_mlflow_tracking()
 
-# Predict on folder
-model.predict(
-    'data/dataset/images/train',
-    project="001-IS-MVTec",
-    save=True,
-)
+    parser = argparse.ArgumentParser(description='YOLO train')
+    parser.add_argument('-c', '--config', type=str, required=True, help='YAML config file')
+    args = parser.parse_args()
+
+    # Load the config file
+    with open(args.config, 'r') as ymlfile:
+        config_file = yaml.load(ymlfile, Loader=yaml.Loader)
+
+
+    # TODO: load weights from mlflow or local
+    # Load a model
+    model = YOLO("001-IS-MVTec/train/weights/best.pt")
+
+    # TODO: run validation on test set
+
+    # Predict on folder
+    model.predict(
+        'data/dataset/images/train',
+        project="001-IS-MVTec",
+        save=True,
+    )
 
